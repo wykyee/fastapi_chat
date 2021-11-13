553c4e00c18858e2296e318a97cc1f008a2b4cbe
Danylo Vasyliuk <danil.vasiliuk@gmail.com>
2021-11-13 19:34:02 +0200

    setup, create package

--- src/chat/settings.py
+++ src/chat/settings.py
@@ -15,7 +15,7 @@ class BrokerSettings(BaseSettings):
     host: str = getenv("CHAT_BROKER_HOST", "0.0.0.0")
     port: int = getenv("CHAT_BROKER_PORT", 6300)
     db: int = getenv("CHAT_BROKER_DB", 2)
-    channel_name: str = getenv("CHAT_CHANNEL_NAME", "chat")
+    channel_name: str = getenv("CHAT_CHANNEL_NAME", "chat_channel")
 
 
 class DjangoServerSettings(BaseSettings):
